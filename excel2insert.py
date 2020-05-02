7eea1b1f201c9681f7a76876d0cb26f9909b52bf
juryokun <capeta.kozo@gmail.com>
2020-05-02 15:13:25 +0900

    Fix logic

--- excel2insert.py
+++ excel2insert.py
@@ -4,6 +4,9 @@ import sys
 
 
 def main():
+    """
+    メイン処理
+    """
     # load excel
     with open('settings.json', 'r') as f:
         try:
@@ -18,27 +21,52 @@ def main():
             print(e)
             sys.exit(1)
 
-    # get table name
-    table_name = get_table_name(sheet)
+    try:
+        # get table name
+        table_name = get_table_name(sheet)
 
-    # get data_types
-    data_types = get_data_types(sheet)
+        # get data_types
+        data_types = get_data_types(sheet)
 
-    # set header
-    write_header(output, table_name, data_types)
+        # set header
+        write_header(output, table_name, data_types)
 
-    # set body
-    write_body(sheet, output, data_types)
+        # set body
+        write_body(sheet, output, data_types)
 
-    # set footer
-    write_footer(output)
+        # set footer
+        write_footer(output)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    sys.exit()
 
 
 def get_table_name(sheet):
+    """
+    テーブル名取得
+
+    Returns
+    ----------
+    str or int or None
+    """
     return get_excel_value(sheet, 1, 2)
 
 
 def get_data_types(sheet):
+    """
+    各カラムの型を取得
+
+    Parameters
+    ----------
+    sheet : object
+        EXCELシートオブジェクト
+
+    Returns
+    ----------
+    data_types : dict => {column_name: data_type}
+    """
     data_types = {}
     excel_column = 1
     while is_exist_value(sheet, 4, excel_column):
@@ -55,10 +83,34 @@ def get_data_types(sheet):
 
 
 def get_excel_value(sheet, row, column):
+    """
+    該当セルの値を取得する
+
+    Parameters
+    ----------
+    sheet : object
+        EXCELシートオブジェクト
+    row : int
+        行番号
+    column : int
+        列番号
+
+    Returns
+    ---------
+    str or int or None
+    """
     return sheet.cell(row=row, column=column).value
 
 
 def is_exist_value(sheet, row, column):
+    """
+    該当セルに値があるかをチェックする
+
+    Returns
+    ----------
+    bool
+        値があれば True、なければ False
+    """
     value = get_excel_value(sheet, row, column)
     if value != None and value != '':
         return True
@@ -66,6 +118,9 @@ def is_exist_value(sheet, row, column):
 
 
 def write_header(output, table_name, data_types):
+    """
+    ヘッダーをファイルに書き込む
+    """
     value = "INSERT INTO " + table_name + "("
     for index, column_name in enumerate(data_types.keys()):
         value = value + column_name
@@ -78,37 +133,87 @@ def write_header(output, table_name, data_types):
 
 
 def write_footer(output):
+    """
+    フッターをファイルに書き込む
+    """
     with open(output, mode='a') as f:
         f.write("\n;")
 
 
 def write_body(sheet, output, data_types):
+    """
+    インサートするデータをファイルに書き込む
+    """
     row = 5
     while is_exist_value(sheet, row, 1):
+        # 最初の行はスキップ
         if row != 5:
             dataset = ",\n    ("
         else:
             dataset = "    ("
 
         for index, value in enumerate(data_types.values()):
+            # 最初の列はスキップ
             if index != 0:
                 dataset = dataset + ","
+
+            # データを取得し適宜形式を変更する
             data = get_excel_value(sheet, row, index+1)
-            if data == "null":
-                dataset = dataset + data
-                continue
-            if data == None or data == "":
-                dataset = dataset + "\'\'"
-                continue
             if value == "var":
-                dataset = dataset + "\'" + str(data) + "\'"
-                continue
-            dataset = dataset + str(data)
+                dataset = dataset + change_variable_format(data)
+            elif value in ("int", "statement"):
+                dataset = dataset + change_raw_format(data)
+            else:
+                raise Exception("型: "+value+" には対応していません")
+
         dataset = dataset + ")"
+
+        # ファイルに書き込む
         with open(output, mode='a') as f:
             f.write(dataset)
 
         row += 1
+
+
+def change_variable_format(data):
+    """
+    var型のフォーマットに変形
+
+    Returns
+    ----------
+    str
+    """
+    if data == "null" or data == None or data == "":
+        return change_except_format(data)
+    return "\'" + str(data) + "\'"
+
+
+def change_raw_format(data):
+    """
+    特に加工の必要ないフォーマットを対象に変形
+        例外ケース以外は文字列に変更するだけ
+
+    Returns
+    ----------
+    str
+    """
+    if data == "null" or data == None or data == "":
+        return change_except_format(data)
+    return str(data)
+
+
+def change_except_format(data):
+    """
+    特殊なデータが入ってる場合の変形処理
+
+    Returns
+    ----------
+    str
+    """
+    if data == "null":
+        return data
+    if data == None or data == "":
+        return "\'\'"
 
 
 main()
